d78e7dd352c62d88a9f0ff70ee4fb5619d8aaea1
Thenappan K L S <thenappankls@gmail.com>
2026-03-17 09:17:33 +0530

    Add files via upload

---
+++ TestSpec.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1731925385894" ID="ID_162659937" MODIFIED="1738564316099" TEXT="Test Specification">
+<icon BUILTIN="full-1"/>
+<node CREATED="1738563936387" ID="ID_1783945561" MODIFIED="1738564249691" POSITION="right" TEXT="RQM Test Template">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1738563982163" ID="ID_797330637" MODIFIED="1738564251883" POSITION="right" TEXT="Update Summary Section">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1738563995377" ID="ID_551860531" MODIFIED="1738564254463" POSITION="right" TEXT="Test Case Name">
+<icon BUILTIN="full-4"/>
+<node CREATED="1738564037277" HGAP="63" ID="ID_1111830053" MODIFIED="1738564389032" TEXT="TC_&lt;MDFxx&gt;_&lt;MDFName&gt;_&lt;ShortMeaningful Test case name&gt;" VSHIFT="36"/>
+</node>
+<node CREATED="1738564168335" ID="ID_1332261755" MODIFIED="1738564256585" POSITION="right" TEXT="Test Case Description">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1731925485728" HGAP="22" ID="ID_10363370" MODIFIED="1738564358712" POSITION="right" TEXT="Test Case Design" VSHIFT="-82">
+<icon BUILTIN="full-6"/>
+<node CREATED="1738564269933" HGAP="22" ID="ID_818591322" MODIFIED="1738564294876" TEXT="Use test case design template" VSHIFT="37"/>
+<node CREATED="1731925540903" ID="ID_1617008574" MODIFIED="1738564298808" TEXT="Use Automation Desk Library Sequence Names"/>
+<node CREATED="1731925522292" HGAP="19" ID="ID_459333324" MODIFIED="1738564303736" TEXT="Use Parameters derived from SRS analysis" VSHIFT="1"/>
+</node>
+</node>
+</map>
